7424a85314f7f14466666d747c17c304e8eb4de5
Jesus Hernan <jesushernan3@gmail.com>
2020-04-18 18:54:55 -0300

    primer commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta
+      name="viewport"
+      content="with=device-with, initial-scale=1, maximum-scale=1"
+    />
+    <link
+      href="https://fonts.googleapis.com/css?family=Roboto"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="main.css">
+
+    <title>titulo sitio</title>
+  </head>
+  <body>
+    <div class="start-page">
+    <h1>CrMoDe</h1>
+    <a href="#">bienvenido</a>
+    </div>
+    <header>
+      <span>HEADER</span>
+      <div class="nav-bar">
+        <span>Nav-Bar</span>
+        <nav class="main-nav">
+          <ul>
+            <li><a href="#">menu 1</a></li>
+            <li><a href="#">menu 2</a></li>
+            <li><a href="#">menu 3</a></li>
+            <li><a href="#">menu 4</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+    <main>
+      <span>MAIN</span>
+      <article>contenido</article>
+    </main>
+    <footer>
+      <span>FOOTER</span>
+    </footer>
+  </body>
+</html>
