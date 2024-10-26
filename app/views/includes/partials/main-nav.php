35fd249dd97523488a0806e56697ba0a67b9fd19
Jesus Hernan <jesushernan3@gmail.com>
2024-10-26 16:55:12 -0300

    correcion del nab

--- app/views/includes/partials/main-nav.php
+++ app/views/includes/partials/main-nav.php
@@ -2,13 +2,9 @@
     
 <ul>
   <li class="text-left"><a href="/sitios-web">Sitios Web</a></li>
-  <li class="logo"><a href="<?php // echo $raiz;?>"><img src="/public/img/logo.png" alt=""></a></li>
+  <li class="logo"><a href="<?php echo $raiz;?>"><img src="/public/img/logo.png" alt=""></a></li>
   <li class="text-right"><a href="/servicios">Servicios</a></li>
 </ul>
-<!-- <a class="logo-mobile" href="<?php // echo $raiz;?>"><img src="/public/img/logo.png" alt=""></a>
-  <ul>
-    <li class="text-left"><a href="/app/views/content/sitios-web">Sitios Web</a></li>
-    <li class="logo"><a href="<?php // echo $raiz;?>"><img src="/public/img/logo.png" alt=""></a></li>
-    <li class="has-text-right"><a href="/app/views/content/servicios-admin">Servicios</a></li>
-  </ul> -->
+<a class="logo-mobile" href="<?php // echo $raiz;?>"><img src="/public/img/logo.png" alt=""></a>
+  
 </nav>
